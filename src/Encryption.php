b219aa16144c9d0d48e244c70ed86149547aad50
Albirs <albis@gmx.ch>
2022-04-26 20:33:51 +0200

    Contao 4.9.x compatibility, replace mcrypt

---
+++ src/Encryption.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Gruschit\Contao\Isotope\Payment\Sepa;
+
+class Encryption
+{
+    const KEY = '12345678901234567890123456789012';
+
+    public function encrypt(string $data): string
+    {
+        $nonce = random_bytes(SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES);
+        $cipherText = sodium_crypto_aead_chacha20poly1305_ietf_encrypt(
+            (string) $data,
+            $nonce,
+            $nonce,
+            self::KEY
+        );
+
+        return $nonce . $cipherText;
+    }
+
+    public function decrypt(string $data): string
+    {
+        $nonce = mb_substr($data, 0, SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES, '8bit');
+        $payload = mb_substr($data, SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES, null, '8bit');
+
+        try {
+            $plainText = sodium_crypto_aead_chacha20poly1305_ietf_decrypt(
+                $payload,
+                $nonce,
+                $nonce,
+                self::KEY
+            );
+        } catch (\SodiumException $e) {
+            $plainText = '';
+        }
+
+        return false !== $plainText ? $plainText : '';
+    }
+}
---
+++ src/GruschitContaoIsotopePaymentSepa.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @package   isotope-payment-sepa-bundle
+ * @author    Michael Gruschwitz <info@grusch-it.de>
+ * @license   LGPL-3.0+
+ * @copyright Michael Gruschwitz 2019
+ */
+
+declare(strict_types=1);
+
+namespace Gruschit\Contao\Isotope\Payment\Sepa;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GruschitContaoIsotopePaymentSepa extends Bundle
+{
+
+}
