280eaf5ade8d04e2067b2eadddd14fa1dbb6fd9d
Michael Gruschwitz <info@grusch-it.de>
2022-04-30 21:16:43 +0200

    Fix bundle has to be loaded after isotope core

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -24,7 +24,7 @@ class Plugin implements BundlePluginInterface
     {
         return [
             BundleConfig::create(AppBundle::class)
-                        ->setLoadAfter([ ContaoCoreBundle::class ])
+                        ->setLoadAfter([ ContaoCoreBundle::class, 'isotope' ])
                         ->setReplace([ 'isotope-sepa' ]),
         ];
     }
