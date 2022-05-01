728d2b5aec3ddd7f33025eb582cd0258038df232
Michael Gruschwitz <info@grusch-it.de>
2022-05-01 19:49:52 +0200

    Fix missing dca palette fields

--- src/Resources/contao/dca/tl_iso_payment.php
+++ src/Resources/contao/dca/tl_iso_payment.php
@@ -11,7 +11,7 @@
  * @copyright Michael Gruschwitz 2015-2017
  */
 
-$GLOBALS['TL_DCA']['tl_iso_payment']['palettes']['sepa'] = '{type_legend},name,label,type;{note_legend:hide},note;{config_legend},new_order_status,minimum_total,maximum_total,countries,shipping_modules,product_types,product_types_condition,config_ids;{gateway_legend},sepa_persist;{price_legend:hide},price,tax_class;{expert_legend:hide},guests,protected;{enabled_legend},enabled';
+$GLOBALS['TL_DCA']['tl_iso_payment']['palettes']['sepa'] = '{type_legend},name,label,type;{note_legend:hide},note;{config_legend},new_order_status,quantity_mode,minimum_quantity,maximum_quantity,minimum_total,maximum_total,countries,shipping_modules,product_types,product_types_condition,config_ids;{gateway_legend},sepa_persist;{price_legend:hide},price,tax_class;{expert_legend:hide},guests,protected;{enabled_legend},enabled';
 
 $GLOBALS['TL_DCA']['tl_iso_payment']['fields']['sepa_persist'] = array
 (
